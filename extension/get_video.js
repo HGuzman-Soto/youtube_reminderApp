5a111f0bfb6b8a12178c4e9341452adc229eb83a
Roberto Enrique Palma <robertopalmaliberty@gmail.com>
2020-12-19 17:17:47 -0800

    Change page color functionality
    Do not think we need this, but I am just going through the tutorial

--- extension/get_video.js
+++ extension/get_video.js
@@ -25,6 +25,22 @@ What I just thought about, is we want to keep information that keeps track of th
 time stamp where we ended a video
 */
 
+let changeColor = document.getElementById('changeColor');
+
+chrome.storage.sync.get('color',function(data) {
+    changeColor.style.backgroundColor = data.color;
+    changeColor.setAttribute('value', data.color);
+});
+
+changeColor.onclick = function(element) {
+    let color = element.target.value;
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.executeScript(
+          tabs[0].id,
+          {code: 'document.body.style.backgroundColor = "' + color + '";'});
+    });
+  };
+
 function get_single_video() {
     let video_url = window.location.href
     return video_url
