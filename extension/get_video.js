afc62809133e39ca47bb1025bb306334076f5881
HGuzman-Soto <soto26938@gmail.com>
2020-12-26 18:07:07 -0800

    finish basics of getting video

--- extension/get_video.js
+++ extension/get_video.js
@@ -1,38 +1,4 @@
 
-
-/* 2 cases
-1) When your viewing an actual video  --> This case is super trivial 
-   - This needs to be activated (or allowed to be) when the youtube url contains 'watch'. 
-For example "youtube.com/watch?************"
-
-2)  When your looking at a playlist 
-   - This needs to be activated (or allowed to be) when the youtube url contains 'playlist'.
-For example "yotuube.com/playlist?list=WL"
-
-
-Here, what I'm doing is going to get the div id - content to get an array of videos
-Then, I'll find the css selectors that denote each individual video
-
-Essentially, I'll be iterating through an array that will allow me to get an array of url videoss
-
-
-
-AND 
-What I just thought about, is we want to keep information that keeps track of the 
-time stamp where we ended a video
-
-Future implementations:
-1) Give user ability to index the videos they want to add to the
-playlist
-
-2) Automatically set it up so that a video after being watched through
-has the option of being removed from the playlist
-
-3) Potential issues with the time stamp
-
-*/
-
-
 class Video{
     constructor() {
         this.title = [];
@@ -40,6 +6,11 @@ class Video{
     }
 }
 
+/*
+Input: None
+Output: A single video object is created which stores the videos url and title
+
+*/
 
 function get_single_video() {
     const selectors = {
@@ -104,6 +75,17 @@ function sel(em, sel) {
 }
 
 
+
+/* 2 cases
+1) When your viewing an actual video  --> This case is super trivial 
+   - This needs to be activated (or allowed to be) when the youtube url contains 'watch'. 
+For example "youtube.com/watch?************"
+
+2)  When your looking at a playlist 
+   - This needs to be activated (or allowed to be) when the youtube url contains 'playlist'.
+For example "yotuube.com/playlist?list=WL"
+*/
+
 function main() {
     let current_url = String(window.location.href)
 
@@ -122,3 +104,35 @@ main()
 
 
 
+
+/* 2 cases
+1) When your viewing an actual video  --> This case is super trivial 
+   - This needs to be activated (or allowed to be) when the youtube url contains 'watch'. 
+For example "youtube.com/watch?************"
+
+2)  When your looking at a playlist 
+   - This needs to be activated (or allowed to be) when the youtube url contains 'playlist'.
+For example "yotuube.com/playlist?list=WL"
+
+
+Here, what I'm doing is going to get the div id - content to get an array of videos
+Then, I'll find the css selectors that denote each individual video
+
+Essentially, I'll be iterating through an array that will allow me to get an array of url videoss
+
+
+
+AND 
+What I just thought about, is we want to keep information that keeps track of the 
+time stamp where we ended a video
+
+Future implementations:
+1) Give user ability to index the videos they want to add to the
+playlist
+
+2) Automatically set it up so that a video after being watched through
+has the option of being removed from the playlist
+
+3) Potential issues with the time stamp
+
+*/
